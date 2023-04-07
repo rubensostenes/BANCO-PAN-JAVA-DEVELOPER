f0329af8ccfd6f9a8f714be1daf288db32ceb24a
rubensostenes <rubenmelo332@gmail.com>
2023-04-07 14:00:51 -0300

    Adicionado a pasta digitalinnovation

---
+++ digitalinnovation/Carro.java
@@ -0,0 +1,47 @@
+package digitalinnovation;
+
+class Carro{
+    
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public Carro(){
+
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+    }
+    
+}
